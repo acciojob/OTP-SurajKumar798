4c5a1ca3bf741babb68c500e28e3c0391a4f03f3
SurajKumar798 <128129426+SurajKumar798@users.noreply.github.com>
2025-06-19 00:07:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+codes.forEach((code, idx) => {
+	code.addEventListener('keydown', (e)=> {
+		if(e.key >= 0 && e.key <= 9){
+			code.value = "";
+			setTimeout(() => {
+				if(idx < codes.length - 1){
+					codes[idx + 1].focus();
+				}
+			}, 10);
+		}else if(e.key === "Backspace"){
+			if(code.value === ""){
+				if(idx > 0) codes[idx - 1].focus();
+			}else{
+				code.value = "";
+			}
+		}
+	});
+	code.addEventListener("input", () => {
+		code.value = code.value.replace(/[^0-9]/g, "");
+	});
+});
